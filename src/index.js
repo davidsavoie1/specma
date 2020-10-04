cb8cd7f7348de4dcfd328ad5d111fd9c6188a5b8
Unknown <david.savoie@softboxsi.com>
2020-10-03 22:00:43 -0400

    Don't export * as default

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-import * as specma from "./index";
-
-export default specma;
-
 export { default as and } from "./and";
 export { default as or } from "./or";
 export { opt, select } from "./selection";
