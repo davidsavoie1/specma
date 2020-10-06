f8473f1ee33a6f8942003c1bf9566af6b01c4778
David <david.savoie@softboxsi.com>
2020-10-05 21:41:29 -0400

    Export helpers

---
+++ src/helpers.js
@@ -0,0 +1,17 @@
+export { getPred } from "./pred";
+export {
+  getDeclaredEntries,
+  getKeySpec,
+  getSpreadSpec,
+  isSpread,
+} from "./spread";
+export {
+  fromEntries,
+  getEntries,
+  getKeys,
+  getSubSpec,
+  isColl,
+  isPred,
+  isSpec,
+  mergePaths,
+} from "./util";
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import * as helpers from "./helpers";
+
+export { helpers };
 export { default as and } from "./and";
 export { default as or } from "./or";
 export { opt, select } from "./selection";
