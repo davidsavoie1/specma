be9d130a417b5694ec63dc1c0159e71fe8d54c5b
David <david.savoie@softboxsi.com>
2020-10-28 21:33:09 -0400

    Use "..." as spread constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 export const KEY_SPEC = Symbol("KEY_SPEC");
 export const OPTIONAL = Symbol("OPTIONAL");
 export const PRED = Symbol("PRED");
-export const SPREAD = Symbol("SPREAD");
+export const SPREAD = "..."; // Symbol("SPREAD");
