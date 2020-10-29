21c72c8d37bf36e58a5f62ae41928faa82b46951
David <david.savoie@softboxsi.com>
2020-10-28 21:32:11 -0400

    Expose helper and utility functions in index

--- next/index.js
+++ next/index.js
@@ -1,9 +1,22 @@
+import { entries, fromMap, get, keys, mergePaths, set, typeOf } from "./util";
+
+export const util = {
+  entries,
+  fromMap,
+  get,
+  keys,
+  mergePaths,
+  set,
+  typeOf,
+};
+
 export { and } from "./and";
 export { check } from "./check";
 export { conform } from "./conform";
 export { isValid } from "./isValid";
 export { or } from "./or";
 export { pair } from "./pair";
-export { opt, select } from "./selection";
-export { spread } from "./spread";
+export { getPred } from "./pred";
+export { isOpt, opt, select } from "./selection";
+export { getSpread, spread } from "./spread";
 export { validate } from "./validate";
