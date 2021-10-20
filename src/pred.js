d2a34474d8bd7ed2c0e9b0a364dbfa9f770ceaa7
David <david.savoie@softboxsi.com>
2021-10-20 15:53:04 -0400

    Remove unused `setPred` exported function

--- src/pred.js
+++ src/pred.js
@@ -1,26 +1,11 @@
-import { fromSpec, toSpec } from "./collSpec.js";
 import { PRED } from "./constants.js";
 import { isResult, resultsRace } from "./results.js";
-import { isColl, isFunc } from "./util.js";
+import { isFunc } from "./util.js";
 import { validatePred } from "./validation.js";
 
 export function getPred(spec) {
   if (isFunc(spec)) return spec;
   return spec && spec[PRED];
-}
-
-/* Associate a predicate spec to a collection
- * by converting the collection to a standard Map spec,
- * combining the function with existing predicate,
- * then converting Map spec back into original collection type. */
-export function setPred(pred, coll) {
-  if (!isColl(coll)) return coll;
-  const spec = toSpec(coll);
-
-  const currPred = spec.get(undefined);
-  spec.set(undefined, currPred ? combinePreds(currPred, pred) : pred);
-
-  return fromSpec(spec, coll);
 }
 
 /* Combine multiple predicate specs into a single function,
